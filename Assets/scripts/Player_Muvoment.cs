5e027e21fb2759471e3f168d59749f4a19f9d92f
nekto-7 <96546650+nekto-7@users.noreply.github.com>
2023-11-03 23:52:10 +0500

    vit2

--- Assets/scripts/Player_Muvoment.cs
+++ Assets/scripts/Player_Muvoment.cs
@@ -27,26 +27,27 @@ public class Player_Muvoment : MonoBehaviour
             {
                 jspeed = jumpForce;
             }
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                statusSprint = true;
-            }
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                statusSprint = false;
-            }
-
-            if (statusSprint)
-            {
-                horizontal = Input.GetAxis("Horizontal") * sprint;
-                vertical = Input.GetAxis("Vertical") * sprint;
-            }
-            else
-            {
-                horizontal = Input.GetAxis("Horizontal") * speed;
-                vertical = Input.GetAxis("Vertical") * speed;
-            }
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            statusSprint = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            statusSprint = false;
+        }
+
+        if (statusSprint)
+        {
+            horizontal = Input.GetAxis("Horizontal") * sprint;
+            vertical = Input.GetAxis("Vertical") * sprint;
+        }
+        else
+        {
+            horizontal = Input.GetAxis("Horizontal") * speed;
+            vertical = Input.GetAxis("Vertical") * speed;
+        }
+
         jspeed += graviry * Time.deltaTime * 3f;
         Vector3 dir = new Vector3(horizontal, jspeed, vertical);
 
