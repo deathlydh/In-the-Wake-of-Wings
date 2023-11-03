a80f1c523ca646b8af1582cd59fa22005cc864d7
deathlydh <70377878+deathlydh@users.noreply.github.com>
2023-11-04 03:04:29 +0500

    feat: add timeline asset and cinemachine. add test cutscene

---
+++ Assets/scripts/TestCutscene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCutscene : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CutsceneManager.Instance.StartCutscene("Cutscene_1");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CutsceneManager.Instance.StartCutscene("Cutscene_2");
+        }
+    }
+}
